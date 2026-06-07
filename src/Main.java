82051556ff3f5beb07be0f17f7aff1873cba830c
Приходько Софія <sofiykapiryet@gmail.com>
2026-06-07 15:48:30 +0300

    Fix formatting issues in Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        iывсnt [][] matrix = new int[3][3];
+        int [][] matrix = new int[3][3];
         int x=0;
 
         for(int[] row:matrix)
